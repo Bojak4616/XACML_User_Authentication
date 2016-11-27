2e251a9621d74edbbc80adcf3ce0c6c856b1ab3d
Jesse Buonanno <jpb4616@rit.edu>
2016-11-27 18:54:51 -0500

    Generic commit to copy updated code to another machine

--- pdp.py
+++ pdp.py
@@ -1,25 +1,84 @@
 #!/usr/bin/python
 
 import os
+import sys
+import hashlib
 from time import sleep
 
-if __name__ == '__main__':
+CONST_PERMIT = """
+<Response>
+  <Result>
+    <Decision>Permit</Decision>
+  </Result>
+</Response>
+"""
 
+CONST_DENY = """
+<Response>
+  <Result>
+    <Decision>Deny</Decision>
+  </Result>
+</Response>
+"""
+
+def parse_policy():
+    try:
+        with open('xacml_policy.xml', 'r') as XML:
+            xml = XML.read()
+    except IOError:
+        print "[!] xacml_policy.xml could not be opened"
+        sys.exit(1)
+
+    # This will return some PARSED xml or vars to send to the PIP for more info
+    # print xml
+    return str(xml)
+
+
+if __name__ == '__main__':
+    path_to_pip = os.getcwd() + '/pdp_pip'
+    path_to_pep = os.getcwd() + '/pep_pdp'
     # Pipes faster than writing to disk
-    # Create a fresh pipe, deleting anything that was previously in there
-    # Limit permissions so that other users can not read the pipe
-    path = os.getcwd() + '/pdp_pip'
-    print path
+
     # Try/Except to prevent someone from create a pipe with the same name to DoS
     try:
-        os.mkfifo(path, 0600)
+        # Create a fresh pipe, deleting anything that was previously in there
+        # Limit permissions so that other users can not read the pipe
+        os.mkfifo(path_to_pip, 0600)
     except OSError:
-        os.unlink(path)
-        os.mkfifo(path, 0600)
+        os.unlink(path_to_pip)
+        os.mkfifo(path_to_pip, 0600)
 
-    with open(path, 'w') as PIPE:
-        PIPE.write("<XML STUFF>")
+    try:
+        while True:
+            # Read request from PEP
+            with open(path_to_pep, 'r') as PIPE:
+                request = PIPE.read()
 
+            #Compare request with parse_policy()
 
-    sleep(10)
-    os.unlink(path)
+            # If bad then write CONST_DENY and continue to next loop
+            pip_vars = ""
+            # Write request to PIP
+            with open(path_to_pip, 'w') as PIPE:
+                PIPE.write(pip_vars)
+
+            with open(path_to_pip, 'r') as PIPE:
+                pip_var_result = PIPE.read()
+
+            # make function to compare against what was requested from PEP
+            # Function should check if pip_var_result is None, if so return None
+
+            with open(path_to_pep, 'w') as PIPE:
+                # if compare function above PERMIT/DENY
+                # PIPE.write(CONST_PERMIT)
+                # PIPE.write(CONST_DENY)
+                pass
+
+    except KeyboardInterrupt:
+        os.unlink(path_to_pip)
+        sys.exit(0)
+
+    except OSError as e:
+        print "[!]Error: " + str(e)
+        print "[!]Fix issue and restart pip.py"
+        sys.exit(1)
--- pip.py
+++ pip.py
@@ -1,16 +1,87 @@
 #!/usr/bin/python
 
+import ldap
 import os
+import sys
+
+
+def parse_pdp(data):
+    # parse and return vars
+    searchAttribute = ""
+
+    return searchAttribute
+
+
+# searchAttribute value should be an array of variables to query ldap
+def fetch_ldap(searchAttribute):
+    l = ldap.initialize('ldap://ldap.auth:389')
+    binddn = "cn=myUserName,ou=GenericID,dc=my,dc=company,dc=com"
+    pw = "myPassword"
+    basedn = "ou=UserUnits,dc=my,dc=company,dc=com"
+    searchFilter = "(&(gidNumber=123456)(objectClass=posixAccount))"
+    #this will scope the entire subtree under UserUnits
+    searchScope = ldap.SCOPE_SUBTREE
+    #Bind to the server
+    try:
+        l.protocol_version = ldap.VERSION3
+        l.simple_bind_s(binddn, pw)
+    except ldap.INVALID_CREDENTIALS:
+        print "Your username or password is incorrect."
+        sys.exit(0)
+    except ldap.LDAPError, e:
+        if type(e.message) == dict and e.message.has_key('desc'):
+            print e.message['desc']
+        else:
+            print e
+        sys.exit(0)
+    try:
+        ldap_result_id = l.search(basedn, searchScope, searchFilter, searchAttribute)
+        result_set = []
+        while True:
+            result_type, result_data = l.result(ldap_result_id, 0)
+            if (result_data == []):
+                break
+            else:
+                ## if you are expecting multiple results you can append them
+                ## otherwise you can just wait until the initial result and break out
+                if result_type == ldap.RES_SEARCH_ENTRY:
+                    result_set.append(result_data)
+        l.unbind_s()
+        return result_set
+    except ldap.LDAPError, e:
+        print e
+
 
 if __name__ == '__main__':
 
-    path = os.getcwd() + '/pdp_pip'
-    PIPE = open(path, 'r')
-    a = ""
-    while True:
-        a = PIPE.read()
-        print a
-        if a:
-            break
+    path_to_pdp = os.getcwd() + '/pdp_pip'
 
-    print "Done"
+    searchAttribute = ['bojak', 'passwordHash', 'whenCreated']
+    results = fetch_ldap(searchAttribute)
+    for result in results:
+        print result
+
+"""
+    try:
+        while True:
+            with open(path_to_pdp, 'r') as PIPE:
+                request = PIPE.read()
+
+            results = fetch_ldap(parse_pdp(request))
+
+            with open(path_to_pdp, 'w') as PIPE:
+                if not results:
+                    PIPE.write("None")
+                    continue
+
+                #this may need to be returned as XML, not sure yet
+                var_string = ""
+                for var in results:
+                    var_string += var + ":"
+
+                PIPE.write(var_string)
+
+    except KeyboardInterrupt:
+        print "Done"
+        sys.exit(0)
+"""
